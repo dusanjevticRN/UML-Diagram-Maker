2667c48ce7c81b1d87cd835c13d60b992bbd09b3
Dušan Jevtić <djevtic2002@gmail.com>
2023-10-23 02:46:01 +0200

    Ispravljen je velik broj logickih gresaka
    - Visak parametra u nekim od konstruktora
    - Obrisan je veliki broj atributa koji se nisu uopste koristili
    - Neke klase su pripadale pogresnim folderima u okviru mvc paterna
    - Obrisan je velik broj bespotrebnih funkcionalnosti, npr duplo postavljanje iste ikonice
    - Struktura koda je ispravljena tako da se postuje trazeni zahtev, odnosno da se AbstractClassyIcon koristi za akcije

--- src/main/java/raf/dsw/classycraft/app/AppCore.java
+++ src/main/java/raf/dsw/classycraft/app/AppCore.java
@@ -2,8 +2,10 @@ package raf.dsw.classycraft.app;
 
 import raf.dsw.classycraft.app.core.ApplicationFramework;
 
-public class AppCore {
-    public static void main(String[] args) {
+public class AppCore
+{
+    public static void main(String[] args)
+    {
         ApplicationFramework appCore = ApplicationFramework.getInstance();
         appCore.initialize();
     }
--- src/main/java/raf/dsw/classycraft/app/gui/swing/view/MyMenyBar.java
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/MyMenyBar.java
@@ -4,26 +4,27 @@ import raf.dsw.classycraft.app.controller.AboutUsAction;
 import raf.dsw.classycraft.app.controller.ExitAction;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
-public class MyMenyBar extends JMenuBar {
-
-    public MyMenyBar(){
+public class MyMenyBar extends JMenuBar
+{
+    public MyMenyBar()
+    {
 
         JMenu fileMenu = new JMenu("File");
         fileMenu.setMnemonic(KeyEvent.VK_F);
-        ExitAction ea = new ExitAction();
-        fileMenu.add(ea);
+
+        ExitAction exitAction = new ExitAction();
+        fileMenu.add(exitAction);
 
         JMenu editMenu = new JMenu("Edit");
         editMenu.setMnemonic(KeyEvent.VK_E);
+
         AboutUsAction aboutUs = new AboutUsAction();
         editMenu.add(aboutUs);
-        add(editMenu);
 
-        add(fileMenu);
-
+        this.add(editMenu);
+        this.add(fileMenu);
     }
 
 }
